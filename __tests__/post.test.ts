fd74234a989e0477d068bd09ae2aae384feacc20
yuum1276 <dyalsla0105@gmail.com>
2024-01-07 23:26:27 +0900

    post test 코드 작성 중

--- __tests__/post.test.ts
+++ __tests__/post.test.ts
@@ -1,41 +1,73 @@
-import { Context } from 'koa';
-import Post from '../src/controllers/post'
 import PostService from '../src/services/postService';
-import { CreatePost } from '../src/interfaces/IPost';
-import { Database } from '../src/database/config';
+import  Post  from '../src/controllers/post';
+import {Database} from '../src/database/config'
+
+// describe('PostService', () => {
+//   let postService;
+//   let db;
+
+//   beforeEach(() => {
+//     db = Database.getInstance();
+//     postService = new PostService();
+//   });
+
+//   test('updatePost should update a post in the database', async () => {
+//     const postId = '1';
+//     const updatePost ={ userId: '1', title: 'Updated Title', content: 'Updated Content'}
+//     const post = await postService.updatePost(updatePost, postId);
+
+//     expect(post).toBeDefined();
+//     expect(post.userId).toBe('1');
+//     expect(post.title).toBe('Updated Title');
+//     expect(post.content).toBe('Updated Content');
+//   });
+
+//   test('deletePost should delete a post from the database', async () => {
+//     const postId = '1';
+//     const userId = '1';
+//     const result = await postService.deletePost(userId, postId);
+
+//     expect(result).toBe(true);
+//   });
+// });
 
 describe('Post', () => {
   let post: Post;
-  let ctx: Context;
-  let createPost: CreatePost;
   let db: Database;
 
   beforeEach(() => {
-    post = new Post();
-    ctx = {} as Context;
-    createPost = {} as CreatePost;
     db = Database.getInstance();
+    post = new Post();
   });
 
-  it('should create a post successfully', async () => {
-    const client = await db.connect();
-    client.query = jest.fn().mockResolvedValueOnce({});
-    ctx.request = {
-      body: createPost,
+  test('updatePost', async () => {
+    const ctx = {
+      params: {
+        postId: '1'
+      },
+      request: {
+        body:{ userId: '1', title: 'Updated Title', content: 'Updated Content'}
+      }
     };
-    await post.createPost(ctx);
-    expect(ctx.body).toEqual({ success: true, post: {} });
-    client.release();
+
+    await post.updatePost(ctx);
+    expect(ctx.request.body.userId).toBe('1');
+    expect(ctx.request.body.title).toBe('Updated Title');
+    expect(ctx.request.body.content).toBe('Updated Content');
   });
 
-  it('should handle errors during post creation', async () => {
-    const client = await db.connect();
-    client.query = jest.fn().mockRejectedValueOnce(new Error('POST 작성 실패'));
-    ctx.request = {
-      body: createPost,
-    };
-    await post.createPost(ctx);
-    expect(ctx.body).toEqual({ success: false, message: 'POST 작성 실패' });
-    client.release();
-  });
+  // test('deletePost should commit a deletion to the database', async () => {
+  //   const ctx = {
+  //     params: {
+  //       postId: '1'
+  //     },
+  //     request: {
+  //       body: '1'
+  //     }
+  //   };
+
+  //   await post.deletePost(ctx);
+
+  //   expect(ctx.request.body.success).toBe(true);
+  // });
 });
