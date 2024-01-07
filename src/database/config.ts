537f4a3d30890f9fb3af89717cc9116d5b2505a4
yuum1276 <dyalsla0105@gmail.com>
2024-01-07 21:36:39 +0900

    db class로 변경

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,14 +1,43 @@
-import pg from "pg";
+import { Pool, QueryResult } from "pg";
 
-export function database() {
-    return new pg.Pool({
-        database: 'postgres_db',
-        user: 'postgres',
-        password: '1234',
-        port: 5432,
-        statement_timeout: 10000,
-        connectionTimeoutMillis: 30000,
-        max: 30,
-    })
+type Query = {
+    text: string;
+    values?: (string | number)[];
+}
+
+export class Database {
+    private static instance: Database;
+    private pool: Pool;
+
+    private constructor() {
+        this.pool = new Pool({
+            database: 'postgres_db',
+            user: 'postgres',
+            password: '1276',
+            port: 5432,
+            statement_timeout: 10000,
+            connectionTimeoutMillis: 30000,
+            max: 30,
+        });
+    }
+
+    public static getInstance(): Database {
+        if (!Database.instance) {
+            Database.instance = new Database();
+        }
+        return Database.instance;
+    }
+
+    public async query(query: Query): Promise<QueryResult> {
+        return this.pool.query(query);
+    }
+
+    public async connect(): Promise<any> {
+        return this.pool.connect();
+    }
+
+    public async end(): Promise<void> {
+        return this.pool.end();
+    }
 }
 
